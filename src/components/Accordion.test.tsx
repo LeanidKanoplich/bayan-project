075ec19ba1795b949ca997c0d57683821d038511
ASSET <asset@MacBook-Pro-ASSET.local>
2024-07-19 01:50:51 +0200

    1render necessary dependencies

--- src/components/Accordion.test.tsx
+++ src/components/Accordion.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom'; // Обновленный импорт
 import Accordion from './Accordion';
 import { Accordion as AccordionType } from '../types';
 
--- src/components/Aside.test.tsx
+++ src/components/Aside.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom'; // Обновленный импорт
 import Aside from './Aside';
 
 test('renders Aside component', () => {
